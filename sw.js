265483a85dafee847e42ae53b86135ae3f1636bb
MioEck <macbook33@googlemail.com>
2026-04-21 16:13:21 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,22 @@
-const CACHE = 'kursplan-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
+var CACHE = 'kursplan-v17';
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(CACHE).then(function(c) {
+    return c.addAll(['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png']);
+  }).then(function(){ return self.skipWaiting(); }));
 });
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
+self.addEventListener('activate', function(e) {
+  e.waitUntil(caches.keys().then(function(keys) {
+    return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
+  }).then(function(){ return self.clients.claim(); }));
 });
-
-self.addEventListener('fetch', e => {
+self.addEventListener('fetch', function(e) {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/index.html')))
+    fetch(e.request).then(function(r) {
+      var clone = r.clone();
+      caches.open(CACHE).then(function(c){ c.put(e.request, clone); });
+      return r;
+    }).catch(function() {
+      return caches.match(e.request).then(function(c){ return c || caches.match('/index.html'); });
+    })
   );
 });
