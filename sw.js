ab910dd29e60daa81ba07256b7be881934def393
MioEck <macbook33@googlemail.com>
2026-05-04 21:43:12 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,26 +1,5 @@
-var CACHE = 'kursplan-v21';
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(CACHE).then(function(c) {
-      return c.addAll(['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png']);
-    }).then(function(){ return self.skipWaiting(); })
-  );
-});
-self.addEventListener('activate', function(e) {
-  e.waitUntil(
-    caches.keys().then(function(keys) {
-      return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
-    }).then(function(){ return self.clients.claim(); })
-  );
-});
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    fetch(e.request).then(function(r) {
-      var clone = r.clone();
-      caches.open(CACHE).then(function(c){ c.put(e.request, clone); });
-      return r;
-    }).catch(function() {
-      return caches.match(e.request).then(function(c){ return c || caches.match('/index.html'); });
-    })
-  );
-});
+const CACHE='kp-v36';
+const FILES=['/Kursplan/','/Kursplan/index.html','/Kursplan/manifest.json','/Kursplan/icon-192.png','/Kursplan/icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
