29e6e38ceef7780f03c2b71dc186d0e9c3897a09
MioEck <macbook33@googlemail.com>
2026-05-05 06:49:08 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kp-v40';
+const CACHE='kp-v41';
 const FILES=['/Kursplan/','/Kursplan/index.html','/Kursplan/manifest.json','/Kursplan/icon-192.png','/Kursplan/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
